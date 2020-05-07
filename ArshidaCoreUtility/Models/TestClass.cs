a73034b6abdbfc9ef520214dc76518e0032c2b0b
Shirzad Bayat <shirzadbayat@gmail.com>
2020-05-07 19:43:07 +0430

    New Class ForTest

---
+++ ArshidaCoreUtility/Models/TestClass.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArshidaCoreUtility.Models
+{
+    public class TestClass
+    {
+        public int id { get; set; }
+    }
+}
